4cef32b4be6d99c014e930264edb6a0b04c29739
JKWTCN <jkwtcn@icloud.com>
2026-07-05 09:52:51 +0800

    feat:增加中文翻译;

---
+++ lang/BatteryMonitor_zh_CN.ts
@@ -0,0 +1,146 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <location filename="../mainwindow.cpp" line="222"/>
+        <location filename="../mainwindow.cpp" line="318"/>
+        <source>Battery Monitor</source>
+        <translation>电池监视器</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="49"/>
+        <source>Device</source>
+        <translation>设备</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="54"/>
+        <source>Type</source>
+        <translation>类型</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="59"/>
+        <source>Battery</source>
+        <translation>电量</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="64"/>
+        <source>Status</source>
+        <translation>状态</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="74"/>
+        <source>Refresh interval:</source>
+        <translation>刷新间隔：</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="82"/>
+        <source>5 seconds</source>
+        <translation>5 秒</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="87"/>
+        <source>10 seconds</source>
+        <translation>10 秒</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="92"/>
+        <source>30 seconds</source>
+        <translation>30 秒</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="97"/>
+        <source>60 seconds</source>
+        <translation>60 秒</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="118"/>
+        <location filename="../mainwindow.cpp" line="226"/>
+        <source>Refresh</source>
+        <translation>刷新</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="61"/>
+        <source>Full</source>
+        <translation>满电</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="62"/>
+        <source>Medium</source>
+        <translation>中等</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="63"/>
+        <source>Low</source>
+        <translation>偏低</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="64"/>
+        <source>Empty</source>
+        <translation>耗尽</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="65"/>
+        <source>Unknown</source>
+        <translation>未知</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="73"/>
+        <source>Wired</source>
+        <translation>有线</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="101"/>
+        <source>Wired power</source>
+        <translation>有线供电</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="104"/>
+        <source>Disconnected</source>
+        <translation>未连接</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="106"/>
+        <source>Connected</source>
+        <translation>已连接</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="202"/>
+        <location filename="../mainwindow.cpp" line="337"/>
+        <source>No devices detected</source>
+        <translation>未检测到设备</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="225"/>
+        <source>Hide</source>
+        <translation>隐藏</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="228"/>
+        <source>Quit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="319"/>
+        <source>The application will keep running in the system tray. Use the tray menu to quit.</source>
+        <translation>程序将继续在系统托盘中运行。请通过托盘菜单退出。</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="412"/>
+        <source>Battery Monitor - No devices</source>
+        <translation>电池监视器 - 无设备</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="450"/>
+        <source>Low Battery</source>
+        <translation>电量不足</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="451"/>
+        <source>%1: %2</source>
+        <translation>%1：%2</translation>
+    </message>
+</context>
+</TS>
